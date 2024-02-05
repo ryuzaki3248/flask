67196cfbf77604548d1046241c7c5b086a9267a4
ryuzaki3248 <gekidannax7@gmail.com>
2024-02-06 00:40:25 +0900

    テンプレートで for によりリストデータを表示させる

--- testapp/views.py
+++ testapp/views.py
@@ -6,6 +6,7 @@ def index():
     my_dict = {
         'insert_something1': 'views.pyのinsert_something1部分です。',
         'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': ['title1', 'title2', 'title3']        
     }
     return render_template('testapp/index.html', my_dict=my_dict)
 
